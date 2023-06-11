c992ae566b47b9f3a3a3c2f96282a7fd883acb55
Johan Nyman <johan.nyman97@gmail.com>
2023-06-11 15:30:01 +0200

    tog bort template i html, la till update/add/remove funktion

--- script.js
+++ script.js
@@ -3,26 +3,49 @@ todoListObject = document.getElementById("todo-list");
 newTodoBtn = document.getElementById("add-btn");
 removeAllBtn = document.getElementById("remove-all");
 
+// Variabler
+const baseApiUrl = "https://dummyjson.com/todos";
+let todoList = JSON.parse(localStorage.getItem("todos"));
+let order = 1;
+// order är ordningen i renderingen
 class Todo {
-	constructor(id, title, description) {
+	constructor(id, title, description, state, check) {
 		this.id = id;
+		this.order = order;
 		this.title = title;
-		this.description = description;
-		this.created = new Date();
-		this.completed = false;
-		this.completedDate = undefined;
+		this.description = description || undefined;
+		this.created = getDate();
+		this.completed = state || false;
+		this.completedDate = check;
 	}
 }
 
-let todoList = JSON.parse(localStorage.getItem("todos") || "[]");
-
-function createTodo() {}
+function addNewTodo() {
+	new Todo(todoList.length + 1, title, description);
+}
+function updateTodo(clicked) {
+	target = clicked.id - 1;
+	if (todoList[target].completed === false) {
+		todoList[target].completed = true;
+		todoList[target].completedDate = getDate();
+	} else {
+		todoList[target].completed = false;
+		todoList[target].completedDate = undefined;
+	}
+	clicked.classList.toggle("done");
+	renderTodos(todoList);
+}
+function removeTodo(clicked) {
+	target = clicked.id - 1;
+	todoList.splice(target, 1);
+	console.log("removed todo with id " + clicked.id);
+	renderTodos(todoList);
+}
 
 function removeAllTodos() {
 	localStorage.clear();
 }
 
-let todo = new Todo("this is title", "this is description");
 function getDate() {
 	let newDate = new Date();
 	let year = newDate.getFullYear();
@@ -38,41 +61,49 @@ function getDate() {
 }
 
 function renderTodos(todoList) {
-	let todoId = 1;
-	if (!todoList) {
-		return;
-	}
+	order = 1;
+	todoListObject.innerHTML = "";
 	todoList.forEach((todo) => {
 		todoItem = document.createElement("li");
 		todoItem.classList.add("todo");
 		if (todo.completed === true) {
 			todoItem.classList.add("done");
 		}
-		todoItem.id = todoId;
+
+		//varje gång jag kallar på render så sätts order till 1 och ökar med iteration, för att jag ska kunna ändra arrayn
+		todoItem.id = order;
 
 		actions = document.createElement("div");
 		actions.classList.add("actions");
 
+		//check och ta-bort knappar
 		checkBtn = '<i class="btn complete-todo" data-feather="check"></i>';
 		removeBtn = '<i class="btn remove-todo" data-feather="x"></i>';
 
 		actions.innerHTML = checkBtn + removeBtn;
+		//eventListener på div'en runt knapparna
 		actions.addEventListener("click", (event) => {
 			let clickedBtn = event.target;
-			console.log(clickedBtn.classList.value);
-			let clicked = event.target.parentElement.parentElement;
+			let clickedTodo = event.target.parentElement.parentElement;
 			if (
-				clicked.classList.contains("todo") &&
+				clickedTodo.classList.contains("todo") &&
 				clickedBtn.classList.contains("complete-todo")
 			) {
-				clicked.classList.toggle("done");
+				updateTodo(clickedTodo);
+			}
+			if (
+				clickedTodo.classList.contains("todo") &&
+				clickedBtn.classList.contains("remove-todo")
+			) {
+				removeTodo(clickedTodo);
 			}
 		});
-
+		// title
 		todoTitle = document.createElement("h3");
 		todoTitle.classList.add("todo-heading");
-		todoTitle.innerHTML = todo.todo;
+		todoTitle.innerHTML = todo.title;
 
+		// description
 		todoDescription = document.createElement("p");
 		todoDescription.classList.add("description");
 		if (!todo.description) {
@@ -80,16 +111,17 @@ function renderTodos(todoList) {
 		} else {
 			todoDescription = todo.description;
 		}
-
+		// Timestamps
 		createdTimestamp = document.createElement("span");
 		createdTimestamp.classList.add("created-timestamp");
-		// Om det inte finns nåt datum så tar den bara dagens datum
-		if (!todo.created) {
-			createdTimestamp.innerHTML = "Created: " + getDate();
-		}
+		createdTimestamp.innerHTML = "CREATED: " + todo.created;
 		completedTimestamp = document.createElement("span");
 		completedTimestamp.classList.add("completed-timestamp");
+		if (todo.completed === true) {
+			completedTimestamp.innerHTML = "DONE: " + todo.completedDate;
+		}
 
+		//appendar
 		todoItem.append(
 			todoTitle,
 			actions,
@@ -98,31 +130,47 @@ function renderTodos(todoList) {
 			completedTimestamp
 		);
 		todoListObject.append(todoItem);
-
-		todoId++;
+		order++;
 	});
+
+	// uppdaterar ikoner
 	feather.replace();
+	localStorage.todos = JSON.stringify(todoList);
 }
 
 function fetchTodos() {
-	fetch("https://dummyjson.com/todos")
+	fetch(baseApiUrl)
 		.then((res) => res.json())
 		.then((todoData) => {
-			todoList = todoData.todos;
+			let data = todoData.todos;
+			data.forEach((todo) => {
+				// Om fetchade todos redan är "done" vid fetch ger jag dom bara dagens datum för "completed" timestamp.
+				let check = todo.completed;
+				if (todo.completed === true) {
+					check = getDate();
+				}
+				// för samstämmighet gör jag nya object av dom fetchade todos'en
+				let currentTodo = new Todo(
+					todo.id,
+					todo.todo,
+					undefined,
+					todo.completed,
+					check
+				);
+				todoList.push(currentTodo);
+			});
 			renderTodos(todoList);
 			localStorage.todos = JSON.stringify(todoList);
 			return todoList;
 		});
 }
 
-let storage = localStorage.getItem("todos");
 function main() {
-	if (!storage) {
-		console.log("hämtade från APIN");
+	// Om localstorage inte har nån data hämtar den från apin. Annars renderar den datan
+	if (!todoList) {
+		todoList = [];
 		fetchTodos();
-		localStorage.todos = JSON.stringify(todoList);
 	} else {
-		console.log("hade data i localstorage");
 		renderTodos(todoList);
 	}
 }
