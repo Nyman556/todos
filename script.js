5213b254672e74327257098f0f8c3f2f0efb1261
Johan Nyman <johan.nyman97@gmail.com>
2023-06-11 07:44:40 +0200

    todo class structure

---
+++ script.js
@@ -0,0 +1,20 @@
+feather.replace();
+
+// DOM Object
+newTodoBtn = document.getElementById("add-btn");
+removeAllBtn = document.getElementById("remove-all");
+
+class Todo {
+	constructor() {
+		this.title = title;
+		this.tag = "Todo";
+		this.description = discription;
+		this.created = new Date();
+		this.done = false;
+		this.doneDate = undefined;
+	}
+}
+
+function createTodo() {}
+
+function removeAllTodos() {}
